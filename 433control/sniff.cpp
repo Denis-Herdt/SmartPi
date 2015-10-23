e7a9d50f75794ae62cdcf2f1eb1394b788679c9c
Denis Herdt <denis.herdt90@gmail.com>
2015-10-23 16:02:58 +0000

    switch all off

--- 433control/sniff.cpp
+++ 433control/sniff.cpp
@@ -1,11 +1,3 @@
-/*
-  RF_Sniffer
-  
-  Hacked from http://code.google.com/p/rc-switch/
-  
-  by @justy to provide a handy RF code sniffer
-*/
-
 #include "RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,15 +11,13 @@ int SetTimer(struct timeval &tv,int sec){
 	gettimeofday(&tv,NULL);
 	tv.tv_sec+=sec;
 	ten = true;
-	printf("trigtime %d\n", tv);
 	return 1;
 }
 int CheckTimer(struct timeval &tv){
 	struct timeval ctv;
 	gettimeofday(&ctv,NULL);
 	if(ctv.tv_sec > tv.tv_sec){
-		printf("curtime %d\n", ctv);
-		printf("check 1\n");
+		printf("Timer triggered\n");
 		return 1;
 	}
 	else{
@@ -37,9 +27,6 @@ int CheckTimer(struct timeval &tv){
 
 int main(int argc, char *argv[]) {
   
-     // This pin is not the first pin on the RPi GPIO header!
-     // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
-     // for more information.
      int PIN_IN = 1;
      int PIN_OUT = 0;
      
@@ -52,7 +39,7 @@ int main(int argc, char *argv[]) {
      struct timeval tv;
      int on = 15033511;
      int off = 15033510;
-     int alloff = 15033514;    
+     int alloff = 15033505;    
      while(1) {
        
       if (mySwitch.available()) {
@@ -67,13 +54,8 @@ int main(int argc, char *argv[]) {
 		}
 		if (value == off){
 			ten = false;
+			printf("stop Timer\n");
 		}
-//		if ((CheckTimer(tv,60)== 1)&&(ten==true)){
-//			mySwitch.enableTransmit(PIN_OUT);
-//			mySwitch.send(alloff , 24);
-//			printf("off");
-//		}
-	  printf("ten: %d", ten);
           printf("Received %i\n", mySwitch.getReceivedValue() );
 	  
           }
@@ -82,13 +64,8 @@ int main(int argc, char *argv[]) {
     	if(ten==true){
 		if(CheckTimer(tv)==1){
 			ten = false;
-			printf("off");
-//			mySwitch.enableTransmit(PIN_OUT);
-//			mySwitch.send(alloff,24);
-//			ten=false;
-//			mySwitch.disableTransmit();
+			printf("All Switches off");
 			system("sudo ./codesend 15033514");
-			printf("off2");
 		}
 	}
   }
